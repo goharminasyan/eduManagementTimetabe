038434c0d1df7d59e74b62298ff4e3f1e5ad8ed8
Gohar Minasyan <goharminasyan80@gmail.com>
2022-09-26 22:19:38 +0400

    Initial commit

---
+++ src/main/java/com/epam/edumanagementtimetabe/EduManagementTimetabeApplication.java
@@ -0,0 +1,13 @@
+package com.epam.edumanagementtimetabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduManagementTimetabeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduManagementTimetabeApplication.class, args);
+	}
+
+}
