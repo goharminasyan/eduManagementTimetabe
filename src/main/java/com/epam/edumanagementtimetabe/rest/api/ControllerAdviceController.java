66b038d19326575a28096a50e3c5380e74777510
Gohar Minasyan <goharminasyan80@gmail.com>
2022-09-27 23:04:33 +0400

    section 4

---
+++ src/main/java/com/epam/edumanagementtimetabe/rest/api/ControllerAdviceController.java
@@ -0,0 +1,58 @@
+package com.epam.edumanagementtimetabe.rest.api;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+
+@ControllerAdvice
+public class ControllerAdviceController {
+
+    @ModelAttribute("activeTab")
+    public String activeTab(HttpServletRequest request) {
+        if (request.getRequestURI().contains("/")) {
+            String[] path = request.getRequestURI().split("/");
+            if (path.length >= 1) {
+                return path[path.length - 1];
+            }
+        }
+        return request.getRequestURI();
+    }
+
+    @ModelAttribute("allUrl")
+    public String allTab(HttpServletRequest request) {
+        if (request.getRequestURI().contains("/")) {
+            String requestURI = request.getRequestURI();
+            return requestURI;
+        }
+        return request.getRequestURI();
+    }
+
+    @ModelAttribute("firstUrl")
+    public String firstUrl(HttpServletRequest request) {
+        if (request.getRequestURI().contains("/")) {
+            String[] path = request.getRequestURI().split("/");
+            if (path.length >= 1) {
+                return path[path.length - (path.length - 1)];
+            }
+        }
+        return request.getRequestURI();
+    }
+
+    @ModelAttribute("middleUrl")
+    public String middleUrl(HttpServletRequest request) {
+        if (request.getRequestURI().contains("/")) {
+            String[] path = request.getRequestURI().split("/");
+            if (path.length > 2) {
+                String decode = URLDecoder.decode(path[path.length - (path.length - 2)], StandardCharsets.UTF_8);
+                return decode;
+            } else if (path.length == 2) {
+                String decoded = URLDecoder.decode(path[path.length - 1], StandardCharsets.UTF_8);
+                return decoded;
+            }
+        }
+        return URLDecoder.decode(request.getRequestURI(), StandardCharsets.UTF_8);
+    }
+}
---
+++ src/main/resources/static/js/popupControl.js
@@ -0,0 +1,16 @@
+function PopUpShow() {
+    $("#popup-container").show();
+    $("#header").css("-webkit-filter", "blur(10px)");
+    $("#header").css("pointer-events", "none");
+    $("#show-btn").css("pointer-events", "none");
+    $("#show-btn").css("-webkit-filter", "blur(1px)");
+
+}
+
+function PopUpHide() {
+    window.location.href = $("#tab").text();
+    $("#header").css("-webkit-filter", "blur(0px)");
+    $("#header").css("pointer-events", "auto");
+    $("#show-btn").css("pointer-events", "auto");
+    $("#show-btn").css("-webkit-filter", "blur(0px)");
+}
