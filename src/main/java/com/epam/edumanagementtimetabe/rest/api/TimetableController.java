b136456bb7fe2328a4bcd9401928cae2c2cdf482
Armine Zakaryan <armzak91@gmail.com>
2022-09-30 20:42:46 +0400

    Added post method and hashmap for monday lessons full of bugs

--- src/main/java/com/epam/edumanagementtimetabe/rest/api/TimetableController.java
+++ src/main/java/com/epam/edumanagementtimetabe/rest/api/TimetableController.java
@@ -10,35 +10,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/timetable")
 public class TimetableController {
 
-   AcademicCourseService academicCourseService;
+    Map<Integer, AcademicCourse> monday = new HashMap<>();
+
+    Integer key = 0;
+    AcademicCourseService academicCourseService;
 
     public TimetableController(AcademicCourseService academicCourseService) {
         this.academicCourseService = academicCourseService;
+    }
+
+
+    @GetMapping("/creation")
+    public String get4_1(Model model) {
+        model.addAttribute("courses", academicCourseService.findAll());
+        return "timetable4-1";
+    }
+
+    @PostMapping("/creation")
+    public String post4_1(Model model, AcademicCourse course) {
+
+        monday.put(++key, course);
+        model.addAttribute("courses", academicCourseService.findAll());
+        model.addAttribute("lessons", monday);
+        return "timetable4-1";
+    }
+
+
+    @GetMapping("/success")
+    public String get4_1_1() {
+        return "timetable4-1-1";
     }
 
     @GetMapping
     public String get4(ModelMap modelMap) {
         modelMap.addAttribute("timetable", "timetable");
         return "timetable4";
-    }
-
-    @GetMapping("/creation")
-    public String get4_1(Model model) {
-      model.addAttribute("courses", academicCourseService.findAll());
-        return "timetable4-1";
-    }
-    @PostMapping("/creation")
-    public String post4_1(Model model) {
-        model.addAttribute("courses", academicCourseService.findAll());
-        return "timetable4-1";
-    }
-
-    @GetMapping("/success")
-    public String get4_1_1() {
-        return "timetable4-1-1";
     }
 }
